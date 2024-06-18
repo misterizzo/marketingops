8007121483bc4927565142542245f64f460d986f
Adarsh Verma <adarsh.srmcem@gmail.com>
2024-06-18 19:26:11 +0530

    working on conference vault task

--- wp-content/plugins/marketing-ops-core/public/partials/templates/conference-vault/conference-vault-taxonomy-archive.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/conference-vault/conference-vault-taxonomy-archive.php
@@ -18,12 +18,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header();
 
-debug( get_queried_object() );
 $term_id          = get_queried_object()->term_id;
-
-// debug( get_queried_object() );
-
-$term_title       = get_field( 'term_title', $term_id );
+$taxonomy         = get_queried_object()->taxonomy;
+$term_title       = get_field( 'term_title', "{$taxonomy}_{$term_id}" );
 $video_query_args = moc_posts_query_args( 'conference-vault' );
 $video_query      = new WP_Query( $video_query_args );
 
