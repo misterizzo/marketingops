9768e47ec9ef6bbebc7808a383e5470fb25750f4
Adarsh Verma <adarsh.srmcem@gmail.com>
2024-06-25 18:20:57 +0530

    working on conference vault

--- wp-content/plugins/marketing-ops-core/public/partials/templates/conference-vault/conference-vault-main.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/conference-vault/conference-vault-main.php
@@ -40,12 +40,12 @@ $page_id      = get_the_ID();
 
 // Get the terms from which the videos should be shown.
 $terms_from_pillar      = get_post_meta( $page_id, 'select_pillar', true );
+$terms_from_pillar      = ( ! empty( $terms_from_pillar ) && is_array( $terms_from_pillar ) ) ? $terms_from_pillar : array();
 $terms_from_conference  = get_post_meta( $page_id, 'select_conference', true );
+$terms_from_conference  = ( ! empty( $terms_from_conference ) && is_array( $terms_from_conference ) ) ? $terms_from_conference : array();
 $terms_from_skill_level = get_post_meta( $page_id, 'select_skill_level', true );
-
-debug( $terms_from_pillar );
-debug( $terms_from_conference );
-debug( $terms_from_skill_level );
+$terms_from_skill_level = ( ! empty( $terms_from_skill_level ) && is_array( $terms_from_skill_level ) ) ? $terms_from_skill_level : array();
+$merged_terms           = array_merge( $terms_from_conference, $terms_from_pillar, $terms_from_skill_level ); // Merge all the terms.
 ?>
 <section class="marketingopstemplatesconfernace conferencevault elementor-section elementor-section-boxed">
 	<div class="margktingimgss"></div>
@@ -135,41 +135,49 @@ debug( $terms_from_skill_level );
 					</div>
 				</div>
 			</div>
-			<div class="conferencevaultinner_innerright">
-				<div class="conferencevaultinner_innerright_inner this_should_repeat">
-					<h2>Mops-A-palooza 2023</h2>
-					<p>83 Sessions</p>
-					<ul>
-						<li>
-							<div class="conferencevaultinnergridboximage">
-								<div class="innerimagebox">
-									<img src="https://marketingops.com/wp-content/uploads/2024/05/Rectangle-868.jpg" />
-								</div>
-								<div class="innerimageboxdescriptions">
-									<h4>Unveiling the New Pillars</h4>
-									<small>by Darrell Alfonso</small>
-									<p>Sem integer vitae justo eget magna fermentum. Arcu dui vivamus arcu felis bibendum. Nibh nisl condimentum id venenatis a condimentum. In arcu cursus euismod quis viverra. </p>
-								</div>
-							</div>
-						</li>
-						<li>
-							<div class="conferencevaultinnergridboximage">
-								<div class="conferencevaultinnergridboximageshowmore">
-									<a href="javascript:void(0);">
-										Show this event
-										<i><svg xmlns="http://www.w3.org/2000/svg" width="15" height="11" viewBox="0 0 15 11" fill="none"><g clip-path="url(#clip0_26_82)"><path d="M10.5262 3.99457C10.2892 3.98546 10.0693 4.12103 9.97249 4.3375C9.87452 4.55396 9.91667 4.80688 10.0807 4.98005L11.8728 6.91682H0.592831C0.382065 6.9134 0.187248 7.02391 0.0812957 7.20619C-0.0257965 7.38734 -0.0257965 7.61292 0.0812957 7.79406C0.187248 7.97634 0.382065 8.08685 0.592831 8.08344H11.8728L10.0807 10.0202C9.9349 10.1729 9.88363 10.3916 9.94515 10.5933C10.0067 10.7949 10.1719 10.9476 10.3769 10.9931C10.5831 11.0387 10.7973 10.9692 10.9375 10.8131L14.001 7.50013L10.9375 4.18711C10.8326 4.0709 10.6834 4.00027 10.5262 3.99457Z" fill="#242730"/></g><defs><clipPath id="clip0_26_82"><rect width="15" height="11" fill="white"/></clipPath></defs></svg></i>
-									</a>
-								</div>
-							</div>
-						</li>
-					</ul>
-				</div>
-				<div class="confernceloadmore">
-					<div class="confernceloadmoreinner">
-						<button type="button"><?php esc_html_e( 'Load More', 'marketing-ops-core' ); ?></button>
+
+			<!-- IF THERE ARE MERGED TERMS -->
+			<?php if ( ! empty( $merged_terms ) && is_array( $merged_terms ) ) { ?>
+				<div class="conferencevaultinner_innerright">
+					<?php foreach ( $merged_terms as $term_id ) {
+						$term = get_term( $term_id );
+						?>
+						<div class="conferencevaultinner_innerright_inner this_should_repeat">
+							<h2><?php echo wp_kses_post( $term->name ); ?></h2>
+							<p>83 Sessions</p>
+							<ul>
+								<li>
+									<div class="conferencevaultinnergridboximage">
+										<div class="innerimagebox">
+											<img src="https://marketingops.com/wp-content/uploads/2024/05/Rectangle-868.jpg" />
+										</div>
+										<div class="innerimageboxdescriptions">
+											<h4>Unveiling the New Pillars</h4>
+											<small>by Darrell Alfonso</small>
+											<p>Sem integer vitae justo eget magna fermentum. Arcu dui vivamus arcu felis bibendum. Nibh nisl condimentum id venenatis a condimentum. In arcu cursus euismod quis viverra. </p>
+										</div>
+									</div>
+								</li>
+								<li>
+									<div class="conferencevaultinnergridboximage">
+										<div class="conferencevaultinnergridboximageshowmore">
+											<a href="javascript:void(0);">
+												Show this event
+												<i><svg xmlns="http://www.w3.org/2000/svg" width="15" height="11" viewBox="0 0 15 11" fill="none"><g clip-path="url(#clip0_26_82)"><path d="M10.5262 3.99457C10.2892 3.98546 10.0693 4.12103 9.97249 4.3375C9.87452 4.55396 9.91667 4.80688 10.0807 4.98005L11.8728 6.91682H0.592831C0.382065 6.9134 0.187248 7.02391 0.0812957 7.20619C-0.0257965 7.38734 -0.0257965 7.61292 0.0812957 7.79406C0.187248 7.97634 0.382065 8.08685 0.592831 8.08344H11.8728L10.0807 10.0202C9.9349 10.1729 9.88363 10.3916 9.94515 10.5933C10.0067 10.7949 10.1719 10.9476 10.3769 10.9931C10.5831 11.0387 10.7973 10.9692 10.9375 10.8131L14.001 7.50013L10.9375 4.18711C10.8326 4.0709 10.6834 4.00027 10.5262 3.99457Z" fill="#242730"/></g><defs><clipPath id="clip0_26_82"><rect width="15" height="11" fill="white"/></clipPath></defs></svg></i>
+											</a>
+										</div>
+									</div>
+								</li>
+							</ul>
+						</div>
+					<?php } ?>
+					<div class="confernceloadmore">
+						<div class="confernceloadmoreinner">
+							<button type="button"><?php esc_html_e( 'Load More', 'marketing-ops-core' ); ?></button>
+						</div>
 					</div>
 				</div>
-			</div>
+			<?php } ?>
 		</div>
 	</div>
 </section>
