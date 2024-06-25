9898d81ca6feec12b204f3aaee5b7ce3c3364c41
Adarsh Verma <adarsh.srmcem@gmail.com>
2024-06-25 15:20:48 +0530

    working on conference vault

--- wp-content/plugins/marketing-ops-core/public/class-marketing-ops-core-public.php
+++ wp-content/plugins/marketing-ops-core/public/class-marketing-ops-core-public.php
@@ -6212,7 +6212,7 @@ class Marketing_Ops_Core_Public {
 	 *
 	 * @since 1.0.0
 	 */
-	public function load_more_conf_videos() {
+	public function mops_load_more_conf_videos_callback() {
 		$page      = filter_input( INPUT_POST, 'page', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
 		$max_pages = filter_input( INPUT_POST, 'max_pages', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
 
