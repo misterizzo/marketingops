24af944d08bdf85fbf8e55e4619c9caaef2558f6
Adarsh Verma <adarsh.srmcem@gmail.com>
2024-06-25 14:48:57 +0530

    working on conference vault

--- wp-content/plugins/marketing-ops-core/public/partials/templates/conference-vault/conference-vault-taxonomy-archive.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/conference-vault/conference-vault-taxonomy-archive.php
@@ -21,12 +21,8 @@ get_header();
 $term_id          = get_queried_object()->term_id;
 $taxonomy         = get_queried_object()->taxonomy;
 $term_title       = get_field( 'term_title', "{$taxonomy}_{$term_id}" );
-$video_query_args = moc_posts_query_args( 'conference_vault' );
+$video_query_args = moc_posts_query_args( 'conference_vault', 1, 16 );
 $video_query      = new WP_Query( $video_query_args );
-
-var_dump( $video_query->found_posts );
-debug( $video_query );
-
 $pillars          = get_terms( // Get the pillars.
 	array(
 		'taxonomy'   => 'pillar',
@@ -45,7 +41,7 @@ $pillars          = get_terms( // Get the pillars.
 						<i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="11" viewBox="0 0 16 11" fill="none"><g clip-path="url(#clip0_26_136)"><path d="M4.97479 2.50043C5.21176 2.49132 5.43164 2.62689 5.52848 2.84335C5.62646 3.05982 5.58431 3.31274 5.42025 3.48591L3.62817 5.42268H14.9081C15.1189 5.41926 15.3137 5.52977 15.4197 5.71205C15.5268 5.8932 15.5268 6.11877 15.4197 6.29992C15.3137 6.4822 15.1189 6.59271 14.9081 6.5893H3.62817L5.42025 8.52607C5.56608 8.67873 5.61735 8.89747 5.55583 9.09912C5.4943 9.30077 5.32911 9.45344 5.12404 9.49901C4.91783 9.54458 4.70365 9.47508 4.56352 9.319L1.5 6.00599L4.56352 2.69297C4.66833 2.57676 4.81757 2.50613 4.97479 2.50043Z" fill="url(#paint0_linear_26_136)"/></g><defs><linearGradient id="paint0_linear_26_136" x1="15.8302" y1="6.02333" x2="-6.76765" y2="6.02333" gradientUnits="userSpaceOnUse"><stop stop-color="#FD4B7A"/><stop offset="1" stop-color="#4D00AE"/></linearGradient><clipPath id="clip0_26_136"><rect width="15" height="10" fill="white" transform="matrix(-1 0 0 1 15.5 0.5)"/></clipPath></defs></svg></i>&nbsp;<?php esc_html_e( 'Back', 'marketing-ops-core' ); ?>
 					</a>
 				</li>
-				<li>83 Sessions</li>
+				<li><?php echo esc_html( printf( _n( '%d session', '%d sessions', $count, 'marketing-ops-core' ), number_format_i18n( $video_query->found_posts ) ) ); ?></li>
 				<li style="visibility:hidden;">
 					<a href="/conference-vault/" title="<?php esc_html_e( 'Conference Vault', 'marketing-ops-core' ); ?>">
 						<i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="11" viewBox="0 0 16 11" fill="none"><g clip-path="url(#clip0_26_136)"><path d="M4.97479 2.50043C5.21176 2.49132 5.43164 2.62689 5.52848 2.84335C5.62646 3.05982 5.58431 3.31274 5.42025 3.48591L3.62817 5.42268H14.9081C15.1189 5.41926 15.3137 5.52977 15.4197 5.71205C15.5268 5.8932 15.5268 6.11877 15.4197 6.29992C15.3137 6.4822 15.1189 6.59271 14.9081 6.5893H3.62817L5.42025 8.52607C5.56608 8.67873 5.61735 8.89747 5.55583 9.09912C5.4943 9.30077 5.32911 9.45344 5.12404 9.49901C4.91783 9.54458 4.70365 9.47508 4.56352 9.319L1.5 6.00599L4.56352 2.69297C4.66833 2.57676 4.81757 2.50613 4.97479 2.50043Z" fill="url(#paint0_linear_26_136)"/></g><defs><linearGradient id="paint0_linear_26_136" x1="15.8302" y1="6.02333" x2="-6.76765" y2="6.02333" gradientUnits="userSpaceOnUse"><stop stop-color="#FD4B7A"/><stop offset="1" stop-color="#4D00AE"/></linearGradient><clipPath id="clip0_26_136"><rect width="15" height="10" fill="white" transform="matrix(-1 0 0 1 15.5 0.5)"/></clipPath></defs></svg></i><?php esc_html_e( 'Prev', 'marketing-ops-core' ); ?>	
