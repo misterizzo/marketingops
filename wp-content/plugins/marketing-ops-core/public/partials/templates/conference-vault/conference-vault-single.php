0ad01ca93bfe3fdc450294e877ae7dc2a9f037f2
Adarsh Verma <adarsh.srmcem@gmail.com>
2024-06-26 19:10:39 +0530

    working on conference vault

--- wp-content/plugins/marketing-ops-core/public/partials/templates/conference-vault/conference-vault-single.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/conference-vault/conference-vault-single.php
@@ -23,6 +23,25 @@ $session_link    = get_field( 'vimeo_video_url', $session_id );
 $conference_term = wp_get_object_terms( $session_id, 'conference' );
 $conference      = ( ! empty( $conference_term[0]->name ) ) ? $conference_term[0]->name : '';
 $back_link       = ( ! empty( $conference_term[0]->term_id ) ) ? get_term_link( $conference_term[0]->term_id ) : '';
+$session_content = get_the_content();
+
+// Get more sessions from the same conference.
+if ( ! empty( $conference_term[0]->taxonomy ) ) {
+	$same_conference_videos_query = moc_get_conference_videos(
+		'conference_vault',
+		1,
+		3,
+		array(
+			'taxonomy' => $conference_term[0]->taxonomy,
+			'field'    => 'term_id',
+			'terms'    => array( $conference_term[0]->term_id ),
+		),
+		''
+	);
+	$same_conference_video_ids    = ( ! empty( $same_conference_videos_query->posts ) ) ? $same_conference_videos_query->posts : '';
+
+	debug( $same_conference_video_ids );
+}
 
 get_header();
 ?>
@@ -36,22 +55,29 @@ get_header();
 					<li>
 						<a href="<?php echo esc_url( $back_link ); ?>">
 							<i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="11" viewBox="0 0 16 11" fill="none"><g clip-path="url(#clip0_26_136)"><path d="M4.97479 2.50043C5.21176 2.49132 5.43164 2.62689 5.52848 2.84335C5.62646 3.05982 5.58431 3.31274 5.42025 3.48591L3.62817 5.42268H14.9081C15.1189 5.41926 15.3137 5.52977 15.4197 5.71205C15.5268 5.8932 15.5268 6.11877 15.4197 6.29992C15.3137 6.4822 15.1189 6.59271 14.9081 6.5893H3.62817L5.42025 8.52607C5.56608 8.67873 5.61735 8.89747 5.55583 9.09912C5.4943 9.30077 5.32911 9.45344 5.12404 9.49901C4.91783 9.54458 4.70365 9.47508 4.56352 9.319L1.5 6.00599L4.56352 2.69297C4.66833 2.57676 4.81757 2.50613 4.97479 2.50043Z" fill="url(#paint0_linear_26_136)"/></g><defs><linearGradient id="paint0_linear_26_136" x1="15.8302" y1="6.02333" x2="-6.76765" y2="6.02333" gradientUnits="userSpaceOnUse"><stop stop-color="#FD4B7A"/><stop offset="1" stop-color="#4D00AE"/></linearGradient><clipPath id="clip0_26_136"><rect width="15" height="10" fill="white" transform="matrix(-1 0 0 1 15.5 0.5)"/></clipPath></defs></svg></i>
-							Back
+							<?php esc_html_e( 'Back', 'marketing-ops-core' ); ?>
 						</a>
 					</li>
 				<?php } ?>
 				<li><?php echo esc_html( sprintf( __( 'by %1$s, %2$s conference', 'marketing-ops-core' ), $session_by, $conference ) ); ?></li>
-				<li style="visibility:hidden;">
-					<a href="javascript:void(0)">
-						<i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="11" viewBox="0 0 16 11" fill="none"><g clip-path="url(#clip0_26_136)"><path d="M4.97479 2.50043C5.21176 2.49132 5.43164 2.62689 5.52848 2.84335C5.62646 3.05982 5.58431 3.31274 5.42025 3.48591L3.62817 5.42268H14.9081C15.1189 5.41926 15.3137 5.52977 15.4197 5.71205C15.5268 5.8932 15.5268 6.11877 15.4197 6.29992C15.3137 6.4822 15.1189 6.59271 14.9081 6.5893H3.62817L5.42025 8.52607C5.56608 8.67873 5.61735 8.89747 5.55583 9.09912C5.4943 9.30077 5.32911 9.45344 5.12404 9.49901C4.91783 9.54458 4.70365 9.47508 4.56352 9.319L1.5 6.00599L4.56352 2.69297C4.66833 2.57676 4.81757 2.50613 4.97479 2.50043Z" fill="url(#paint0_linear_26_136)"/></g><defs><linearGradient id="paint0_linear_26_136" x1="15.8302" y1="6.02333" x2="-6.76765" y2="6.02333" gradientUnits="userSpaceOnUse"><stop stop-color="#FD4B7A"/><stop offset="1" stop-color="#4D00AE"/></linearGradient><clipPath id="clip0_26_136"><rect width="15" height="10" fill="white" transform="matrix(-1 0 0 1 15.5 0.5)"/></clipPath></defs></svg></i>
-						Prev
-					</a>
-				</li>
+				
+				<?php if ( ! empty( $back_link ) ) { ?>
+					<li style="visibility:hidden;">
+						<a href="<?php echo esc_url( $back_link ); ?>">
+							<i><svg xmlns="http://www.w3.org/2000/svg" width="16" height="11" viewBox="0 0 16 11" fill="none"><g clip-path="url(#clip0_26_136)"><path d="M4.97479 2.50043C5.21176 2.49132 5.43164 2.62689 5.52848 2.84335C5.62646 3.05982 5.58431 3.31274 5.42025 3.48591L3.62817 5.42268H14.9081C15.1189 5.41926 15.3137 5.52977 15.4197 5.71205C15.5268 5.8932 15.5268 6.11877 15.4197 6.29992C15.3137 6.4822 15.1189 6.59271 14.9081 6.5893H3.62817L5.42025 8.52607C5.56608 8.67873 5.61735 8.89747 5.55583 9.09912C5.4943 9.30077 5.32911 9.45344 5.12404 9.49901C4.91783 9.54458 4.70365 9.47508 4.56352 9.319L1.5 6.00599L4.56352 2.69297C4.66833 2.57676 4.81757 2.50613 4.97479 2.50043Z" fill="url(#paint0_linear_26_136)"/></g><defs><linearGradient id="paint0_linear_26_136" x1="15.8302" y1="6.02333" x2="-6.76765" y2="6.02333" gradientUnits="userSpaceOnUse"><stop stop-color="#FD4B7A"/><stop offset="1" stop-color="#4D00AE"/></linearGradient><clipPath id="clip0_26_136"><rect width="15" height="10" fill="white" transform="matrix(-1 0 0 1 15.5 0.5)"/></clipPath></defs></svg></i>
+							<?php esc_html_e( 'Prev', 'marketing-ops-core' ); ?>
+						</a>
+					</li>
+				<?php } ?>
 			</ul>
-			<p>An annual marketing plan is a comprehensive document that delineates the strategic roadmap for marketing activities over the course of a year. It serves as a blueprint for how a company will reach its target audience, achieve its business objectives, and drive revenue growth through various promotional efforts. The plan typically includes an in-depth analysis of the market landscape, competitor insights, target customer profiles, and industry trends to inform decision-making.</p>
-			<div class="elementor-wrapper elementor-open-inline">
-				<iframe class="elementor-video-iframe" allowfullscreen="" allow="clipboard-write" title="vimeo Video Player" src="https://player.vimeo.com/video/698695898?color&amp;autopause=0&amp;loop=0&amp;muted=0&amp;title=1&amp;portrait=1&amp;byline=1&amp;h=427b06ed1e#t="></iframe> 
-			</div>
+			<?php echo wp_kses_post( $session_content ); ?>
+
+			<!-- SESSION VIDEO -->
+			<?php if ( ! empty( $session_link ) ) { ?>
+				<div class="elementor-wrapper elementor-open-inline">
+					<iframe class="elementor-video-iframe" allowfullscreen="" allow="clipboard-write" title="vimeo Video Player" src="<?php echo esc_url( $session_link ); ?>?color&amp;autopause=0&amp;loop=0&amp;muted=0&amp;title=1&amp;portrait=1&amp;byline=1&amp;h=427b06ed1e#t="></iframe> 
+				</div>
+			<?php } ?>
 			<p><strong>Transcript:</strong>Michael Hartmann: [00:00:00] Welcome to another episode of Opscasts brought to you by MarketingOps.com powered by the MoPros. I’m your host, Michael Hartmann. Flying solo today, which is all good. Uh, Mike and Naomi will be back soon, I’m sure. All right. For this episode, it’s the first of a series that we’re working on that is going to help educate our audience about some emerging go to market approaches and help provide guidance on how you as marketing professional is going to be an enabler.        	</p>
 			<p>Or an advocate, even for some of these approaches at your organization to join us in that conversation for this first one is Justin Gray. We’re going to be talking about near bound go to market strategies. Justin is an award winning five time entrepreneur who has made a career of launching and scaling companies and guiding them to successful equity exits of over 250 million in 2018.</p>
 			<p>He started angel investing and to make strategic investments in founders. He believes in, and is currently a limited partner in several funds. He is now also co founder and managing director of in revenue capital, where he couples go to market [00:01:00] expertise with venture funding to empower seed stage founders and their startups through a first of its kind model called operator immersive capital, uh, he is a strong voice of.</p>
