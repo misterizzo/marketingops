0c3fb99cfa70e196bfd242ba243cae4113e5009b
Adarsh Verma <adarsh.srmcem@gmail.com>
2024-06-26 14:11:29 +0530

    working on conference vault

--- wp-content/plugins/marketing-ops-core/public/partials/templates/conference-vault/conference-vault-main.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/conference-vault/conference-vault-main.php
@@ -47,9 +47,14 @@ if ( ! ( is_null( $get_conference ) || is_null( $get_pillar ) || is_null( $get_c
 	$get_conference             = ( ! is_null( $get_conference ) ) ? explode( '|', $get_conference ) : array();
 	$get_pillar                 = ( ! is_null( $get_pillar ) ) ? explode( '|', $get_pillar ) : array();
 	$get_conference_skill_level = ( ! is_null( $get_conference_skill_level ) ) ? explode( '|', $get_conference_skill_level ) : array();
-	debug( $get_conference );
-	debug( $get_pillar );
-	debug( $get_conference_skill_level );
+
+	// Loop through the conference taxonomy to collect the term IDs.
+	if ( ! empty( $get_conference ) && is_array( $get_conference ) ) {
+		foreach ( $get_conference as $term_slug ) {
+			$conference_term = get_term_by( 'slug', $term_slug, 'conference' );
+			debug( $conference_term ); die;
+		}
+	}
 } else {
 	$terms_from_pillar        = get_post_meta( $page_id, 'select_pillar', true ); // Get the terms from which the videos should be shown.
 	$terms_from_pillar        = ( ! empty( $terms_from_pillar ) && is_array( $terms_from_pillar ) ) ? array_map( 'intval', $terms_from_pillar ) : array();
