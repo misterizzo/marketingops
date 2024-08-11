bd9da341c2ab8f24b7006622b979dd9aac1d3d62
meggira <kumbharanameghna@gmail.com>
2024-08-11 16:18:05 +0530

    add images

--- wp-content/plugins/marketing-ops-core/public/partials/templates/agency/list.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/agency/list.php
@@ -50,7 +50,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/allinclusive.png" alt="img" />
 						<h3>All Inclusive Marketing Inc.</h3>
 						<ul>
 							<li> Affiliate Marketing </li>
@@ -69,7 +69,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/dmipartners.png" alt="img" />
 						<h3>DMi Partners</h3>
 						<ul>
 
@@ -91,7 +91,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/allison.png" alt="img" />
 						<h3>Allison</h3>
 						<ul>
 
@@ -113,7 +113,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/eaccountable.png" alt="img" />
 						<h3>eAccountable</h3>
 						<ul>
 						
@@ -138,7 +138,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/LTPartners.png" alt="img" />
 						<h3>LT Partners</h3>
 						<ul>
 						
@@ -155,7 +155,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/LTPartners.png" alt="img" />
 						<h3>LT Partners</h3>
 						<ul>
 						
@@ -172,7 +172,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/thedigitalvoice.png" alt="img" />
 						<h3>The Digital Voice</h3>
 						<ul>
 						
@@ -190,7 +190,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/thepartneragency.png" alt="img" />
 						<h3>The Partner Agency - B2B Affiliate</h3>
 						<ul>
 						
@@ -212,13 +212,8 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/amdx.png" alt="img" />
 						<h3>Affiliate Marketing Dynamics</h3>
-						<ul>
-						
-							
-							
-						</ul>
 						<a href="javascript:void(0);" class="learnmorebtnagency">
 							Learn more <i class="lernmoresvg"> <svg xmlns="http://www.w3.org/2000/svg" width="15" height="11" viewBox="0 0 15 11" fill="none"><g clip-path="url(#clip0_53_583)"><path d="M10.5262 3.99457C10.2892 3.98546 10.0693 4.12103 9.97249 4.3375C9.87452 4.55396 9.91667 4.80688 10.0807 4.98005L11.8728 6.91682H0.592831C0.382065 6.9134 0.187248 7.02391 0.0812957 7.20619C-0.0257965 7.38734 -0.0257965 7.61292 0.0812957 7.79406C0.187248 7.97634 0.382065 8.08685 0.592831 8.08344H11.8728L10.0807 10.0202C9.9349 10.1729 9.88363 10.3916 9.94515 10.5933C10.0067 10.7949 10.1719 10.9476 10.3769 10.9931C10.5831 11.0387 10.7973 10.9692 10.9375 10.8131L14.001 7.50013L10.9375 4.18711C10.8326 4.0709 10.6834 4.00027 10.5262 3.99457Z" fill="#45474F"/></g><defs><clipPath id="clip0_53_583"><rect width="15" height="11" fill="white"/></clipPath></defs></svg> </i>	
 						</a>
@@ -228,7 +223,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/affiverse.png" alt="img" />
 						<h3>Affiverse</h3>
 						<ul>
 						
@@ -267,7 +262,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/roundbarn.png" alt="img" />
 						<h3>ROUND BARN LABS</h3>
 						<ul>
 						
@@ -285,7 +280,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/LTPartners.png" alt="img" />
 						<h3>LT Partners</h3>
 						<ul>
 						
@@ -302,7 +297,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/eaccountable.png" alt="img" />
 						<h3>eAccountable</h3>
 						<ul>
 						
@@ -327,7 +322,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/thedigitalvoice.png" alt="img" />
 						<h3>The Digital Voice</h3>
 						<ul>
 						
@@ -368,7 +363,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/allinclusive.png" alt="img" />
 						<h3>All Inclusive Marketing Inc.</h3>
 						<ul>
 							<li> Affiliate Marketing </li>
@@ -387,7 +382,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/dmipartners.png" alt="img" />
 						<h3>DMi Partners</h3>
 						<ul>
 
@@ -409,7 +404,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/allison.png" alt="img" />
 						<h3>Allison</h3>
 						<ul>
 
@@ -455,7 +450,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/allinclusive.png" alt="img" />
 						<h3>All Inclusive Marketing Inc.</h3>
 						<ul>
 							<li> Affiliate Marketing </li>
@@ -474,7 +469,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/dmipartners.png" alt="img" />
 						<h3>DMi Partners</h3>
 						<ul>
 
@@ -496,7 +491,7 @@ get_header();
 				<li>
 					<div class="inneragencylistbox">
 						<h4>PARTNER</h4>
-						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/acelleration.png" alt="img" />
+						<img src="/wp-content/themes/hello-elementor_child/images/agencypages/allison.png" alt="img" />
 						<h3>Allison</h3>
 						<ul>
 
@@ -514,12 +509,6 @@ get_header();
 						</a>
 					</div>	
 				</li>
-
-
-
-
-
-
 			</ul>
 		</div>	
 	</div>
