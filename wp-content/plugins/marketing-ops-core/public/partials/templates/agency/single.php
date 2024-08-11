75bcf1ec5c5aeabfc395800e034264a1d16052a4
meggira <kumbharanameghna@gmail.com>
2024-08-11 19:55:23 +0530

    pagination

--- wp-content/plugins/marketing-ops-core/public/partials/templates/agency/single.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/agency/single.php
@@ -17,6 +17,45 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header();
 ?>
-<p>agency single html starts here...</p>
+<section class="agaeny_clients">
+	<div class="agency-container">
+		<h3>Clients</h3>
+		<p>
+			Allbirds, GoTo, Noom, Truff, Target, Legacy Box, Instacart, Redbubble, Blenders Eyewear, Crocs, Umbra, Helly Hansen, Eight Sleep, Mint Mobile, Third Love, Dick's Sporting Goods, Drop, Sizzlefish, DiscountMugs, Nutrafol, Proactiv, Hawthorne, HydroPeptide, Crossrope
+		</p>
+	</div>	
+</section>
+
+<section class="agaeny_clients">
+	<div class="agency-container">
+		<h3>Certifications</h3>
+		<p>
+		Impact, CJ, Awin/ShareASale, Rakuten, Partnerize
+		</p>
+	</div>	
+</section>
+
+<section class="agaeny_clients">
+	<div class="agency-container">
+		<h3>Awards</h3>
+		<ul>
+			<li>
+				Best Travel, Leisure and Lifestyle Campaign - London, 2020 International Performance Marketing Awards
+			</li>	
+			<li>
+				Best B2B Strategy, USA - 2022 Highly Commended, International Performance Marketing Awards
+			</li>
+			<li>
+				Top 20 Digital Strategy Leaders of 2020 - Digital Strategy Institute	
+			</li>	
+			<li>
+				GROWTH500 - 2019 Top 500 Fastest Growing Companies in Canada, Canadian Business Magazine
+			</li>
+			<li>
+				Global Excellence Award - London, 2018 Performance Marketing Awards
+			</li>
+		</ul>
+	</div>	
+</section>
 <?php
 get_footer();
