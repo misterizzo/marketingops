cb009a3f3b0ff3d1041e9e33657c930fed6cab40
meggira <kumbharanameghna@gmail.com>
2024-08-11 17:13:10 +0530

    add images

--- wp-content/plugins/marketing-ops-core/public/partials/templates/agency/list.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/agency/list.php
@@ -18,9 +18,54 @@ if ( ! defined( 'ABSPATH' ) ) {
 get_header();
 ?>
 <section class="agenctdirectoryblock">
+	<div class="leftbgbar">
+		<img src="/wp-content/themes/hello-elementor_child/images/agencypages/blurcircle1.png" alt="img" />
+	</div>
+	<div class="leftbgbar_two">
+		<img src="/wp-content/themes/hello-elementor_child/images/agencypages/blur3.png" alt="img" />
+	</div>
+	<div class="rightbgbar">
+		<img src="/wp-content/themes/hello-elementor_child/images/agencypages/blur2.png" alt="img" />
+	</div>
+	<div class="rightbgbar_two">
+		<img src="/wp-content/themes/hello-elementor_child/images/agencypages/blurcircle1.png" alt="img" />
+	</div>
 	<div class="agency-container">
 		<h1>Agency Directory</h1>
 		<h2>Curated list of Wubba Lubba dub-dub agencies, top of the line. </h2>
+		<div id="container" class="agency-containe">
+			<select id="normal-select-1" placeholder-text="Item 1">
+				<option value="1" class="select-dropdown__list-item">Item 1</option>
+				<option value="2" class="select-dropdown__list-item">Item 2</option>
+				<option value="3" class="select-dropdown__list-item">Item 3</option>
+				<option value="4" class="select-dropdown__list-item">Item 4</option>
+			</select>
+			
+			<select id="normal-select-2" placeholder-text="Item 1">
+				<option value="1" class="select-dropdown__list-item">Item 1</option>
+				<option value="2" class="select-dropdown__list-item">Item 2</option>
+				<option value="3" class="select-dropdown__list-item">Item 3</option>
+				<option value="4" class="select-dropdown__list-item">Item 4</option>
+				<option value="5" class="select-dropdown__list-item">Item 5</option>
+				<option value="6" class="select-dropdown__list-item">Item 6</option>
+			</select>
+
+			<select id="normal-select-3" placeholder-text="Item 1">
+				<option value="1" class="select-dropdown__list-item">Item 1</option>
+				<option value="2" class="select-dropdown__list-item">Item 2</option>
+				<option value="3" class="select-dropdown__list-item">Item 3</option>
+				<option value="4" class="select-dropdown__list-item">Item 4</option>
+			</select>
+			
+			<select id="normal-select-4" placeholder-text="Item 1">
+				<option value="1" class="select-dropdown__list-item">Item 1</option>
+				<option value="2" class="select-dropdown__list-item">Item 2</option>
+				<option value="3" class="select-dropdown__list-item">Item 3</option>
+				<option value="4" class="select-dropdown__list-item">Item 4</option>
+				<option value="5" class="select-dropdown__list-item">Item 5</option>
+				<option value="6" class="select-dropdown__list-item">Item 6</option>
+			</select>
+		</div>
 
 		<div class="agency-mainlistboxs">
 			<ul class="innermainlistboxs">
@@ -633,5 +678,95 @@ get_header();
 		</div>	
 	</div>
 </section>
+
+
+<script>
+
+document.addEventListener('DOMContentLoaded', createSelect, false);
+function createSelect() {
+    var select = document.getElementsByTagName('select'),
+      liElement,
+      ulElement,
+      optionValue,
+      iElement,
+      optionText,
+      selectDropdown,
+      elementParentSpan;
+      for (var select_i = 0, len = select.length; select_i < len; select_i++) {
+      select[select_i].style.display = 'none';
+      wrapElement(document.getElementById(select[select_i].id), document.createElement('div'), select_i, select[select_i].getAttribute('placeholder-text'));
+      for (var i = 0; i < select[select_i].options.length; i++) {
+        liElement = document.createElement("li");
+        optionValue = select[select_i].options[i].value;
+        optionText = document.createTextNode(select[select_i].options[i].text);
+        liElement.className = 'select-dropdown__list-item';
+        liElement.setAttribute('data-value', optionValue);
+        liElement.appendChild(optionText);
+        ulElement.appendChild(liElement);
+        liElement.addEventListener('click', function () {
+          displyUl(this);
+        }, false);
+      }
+    }
+    function wrapElement(el, wrapper, i, placeholder) {
+      el.parentNode.insertBefore(wrapper, el);
+      wrapper.appendChild(el);
+      document.addEventListener('click', function (e) {
+        let clickInside = wrapper.contains(e.target);
+        if (!clickInside) {
+          let menu = wrapper.getElementsByClassName('select-dropdown__list');
+          menu[0].classList.remove('active');
+        }
+      });
+
+      var buttonElement = document.createElement("button"),
+        spanElement = document.createElement("span"),
+        spanText = document.createTextNode(placeholder);
+        iElement = document.createElement("i");
+        ulElement = document.createElement("ul");
+      wrapper.className = 'select-dropdown select-dropdown--' + i;
+      buttonElement.className = 'select-dropdown__button select-dropdown__button--' + i;
+      buttonElement.setAttribute('data-value', '');
+      buttonElement.setAttribute('type', 'button');
+      spanElement.className = 'select-dropdown select-dropdown--' + i;
+      iElement.className = 'zmdi zmdi-chevron-down';
+      ulElement.className = 'select-dropdown__list select-dropdown__list--' + i;
+      ulElement.id = 'select-dropdown__list-' + i;
+      wrapper.appendChild(buttonElement);
+      spanElement.appendChild(spanText);
+      buttonElement.appendChild(spanElement);
+      buttonElement.appendChild(iElement);
+      wrapper.appendChild(ulElement);
+    }
+
+    function displyUl(element) {
+      if (element.tagName == 'BUTTON') {
+        selectDropdown = element.parentNode.getElementsByTagName('ul');
+        for (var i = 0, len = selectDropdown.length; i < len; i++) {
+          selectDropdown[i].classList.toggle("active");
+        }
+      } else if (element.tagName == 'LI') {
+        var selectId = element.parentNode.parentNode.getElementsByTagName('select')[0];
+        selectElement(selectId.id, element.getAttribute('data-value'));
+        elementParentSpan = element.parentNode.parentNode.getElementsByTagName('span');
+        element.parentNode.classList.toggle("active");
+        elementParentSpan[0].textContent = element.textContent;
+        elementParentSpan[0].parentNode.setAttribute('data-value', element.getAttribute('data-value'));
+      }
+    }
+    function selectElement(id, valueToSelect) {
+      var element = document.getElementById(id);
+      element.value = valueToSelect;
+      element.setAttribute('selected', 'selected');
+    }
+    var buttonSelect = document.getElementsByClassName('select-dropdown__button');
+    for (var i = 0, len = buttonSelect.length; i < len; i++) {
+      buttonSelect[i].addEventListener('click', function (e) {
+				e.preventDefault();
+				displyUl(this);
+			}, false);
+		}
+}
+</script>	
 <?php
 get_footer();
