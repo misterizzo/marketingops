7fbba668740b56f2551e700077692f07b4d95e97
meggira <kumbharanameghna@gmail.com>
2024-08-12 14:37:10 +0530

    agency form

--- wp-content/plugins/marketing-ops-core/public/partials/templates/woocommerce/myaccount/platform-profile.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/woocommerce/myaccount/platform-profile.php
@@ -94,7 +94,7 @@ defined( 'ABSPATH' ) || exit; // Exit if accessed directly.
             </label>
         </p>
 
-        <h5>What services do you provide?  <i><svg xmlns="http://www.w3.org/2000/svg" width="9" height="21" viewBox="0 0 9 21" fill="none"><path d="M3.2 6H5.45L5.325 8.675L7.55 7.275L8.675 9.175L6.3 10.425L8.65 11.725L7.525 13.6L5.325 12.175L5.425 14.7H3.175L3.325 12.2L1.2 13.55L0.075 11.65L2.325 10.4L0 9.125L1.15 7.225L3.325 8.6L3.2 6Z" fill="url(#paint0_linear_34_3644)"/><defs><linearGradient id="paint0_linear_34_3644" x1="-0.204631" y1="10.371" x2="13.798" y2="10.371" gradientUnits="userSpaceOnUse"><stop stop-color="#FD4B7A"/><stop offset="1" stop-color="#4D00AE"/></linearGradient></defs></svg></i></h5>
+        <h5 style="margin-top: 30px !important;">What services do you provide?  <i><svg xmlns="http://www.w3.org/2000/svg" width="9" height="21" viewBox="0 0 9 21" fill="none"><path d="M3.2 6H5.45L5.325 8.675L7.55 7.275L8.675 9.175L6.3 10.425L8.65 11.725L7.525 13.6L5.325 12.175L5.425 14.7H3.175L3.325 12.2L1.2 13.55L0.075 11.65L2.325 10.4L0 9.125L1.15 7.225L3.325 8.6L3.2 6Z" fill="url(#paint0_linear_34_3644)"/><defs><linearGradient id="paint0_linear_34_3644" x1="-0.204631" y1="10.371" x2="13.798" y2="10.371" gradientUnits="userSpaceOnUse"><stop stop-color="#FD4B7A"/><stop offset="1" stop-color="#4D00AE"/></linearGradient></defs></svg></i></h5>
         <div class="agencyformgroup form-group">
             <input type="checkbox" id="Integration">
             <label for="Integration">Program Set up & Integration</label>
