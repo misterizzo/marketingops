127ff24b3af149c205cddf75aa0771ef7a116b30
Adarsh Verma <adarsh.srmcem@gmail.com>
2024-09-27 23:54:48 +0530

    deleted image optimization plugin

--- wp-content/plugins/image-optimization/assets/build/admin.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '223b0510f0669dfe7afb');
--- wp-content/plugins/image-optimization/assets/build/control.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-a11y', 'wp-api-fetch', 'wp-escape-html', 'wp-i18n'), 'version' => 'fbafd726e64c78f54260');
