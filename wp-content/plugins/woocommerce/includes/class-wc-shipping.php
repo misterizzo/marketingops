4d17867bf6ba143b81c05e6f5da6aff943dd05b8
Adarsh Verma <adarsh@concatstring.com>
2024-10-11 20:37:39 +0530

    troubleshoot flat rate shipping bug

--- wp-content/plugins/woocommerce/includes/class-wc-shipping.php
+++ wp-content/plugins/woocommerce/includes/class-wc-shipping.php
@@ -344,11 +344,9 @@ class WC_Shipping {
 					 */
 					do_action( 'woocommerce_before_get_rates_for_package', $package, $shipping_method );
 
-					if ( '183.82.161.187' === $_SERVER['REMOTE_ADDR'] ) {
-						debug( $package );
-						debug( '-----' );
-						debug( $shipping_method->get_rates_for_package( $package ) );
-					}
+					// if ( '183.82.161.187' === $_SERVER['REMOTE_ADDR'] ) {
+					// 	debug( $shipping_method->get_rates_for_package( $package ) );
+					// }
 
 					// Use + instead of array_merge to maintain numeric keys.
 					$package['rates'] = $package['rates'] + $shipping_method->get_rates_for_package( $package );
