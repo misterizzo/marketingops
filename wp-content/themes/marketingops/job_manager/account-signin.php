01204fdb0f3d37febea046cb417e0a701778ebab
Adarsh Verma <adarsh@concatstring.com>
2024-10-14 16:54:28 +0530

    troubleshooting job posting issue

--- wp-content/themes/marketingops/job_manager/account-signin.php
+++ wp-content/themes/marketingops/job_manager/account-signin.php
@@ -17,31 +17,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $post;
 
-if ( '183.82.161.187' === $_SERVER['REMOTE_ADDR'] ) {
-	debug( $post->post_name );
-}
+$current_page_slug = ( ! empty( $post->post_name ) ) ? $post->post_name : '';
 
-if ( is_user_logged_in() ) : ?>
+if ( is_user_logged_in() ) { ?>
 	<div class="pyj_login_form pyj_logged_form">
 		<fieldset class="fieldset-logged_in">
-			<label><?php esc_html_e( 'Your account', 'wp-job-manager' ); ?></label>
+			<label><?php esc_html_e( 'Your account', 'marketingops' ); ?></label>
 			<div class="field account-sign-in">
 				<?php
 					$user = wp_get_current_user();
 					$username = moc_user_display_name( get_current_user_id() );
 					// translators: Placeholder %s is the username.
-					printf( wp_kses_post( __( 'You are currently signed in as <strong>%s</strong>.', 'wp-job-manager' ) ), esc_html( $username ) );
+					printf( wp_kses_post( __( 'You are currently signed in as <strong>%s</strong>.', 'marketingops' ) ), esc_html( $username ) );
 				?>
 
 				<div class="pyj_login_form_btn">
 					<a class="button" href="<?php echo esc_url( apply_filters( 'submit_job_form_logout_url', wp_logout_url( get_permalink() ) ) ); ?>">
-						<span class="form_btn_text"><?php esc_html_e( 'Sign Out', 'wp-job-manager' ); ?></span>
+						<span class="form_btn_text"><?php esc_html_e( 'Sign Out', 'marketingops' ); ?></span>
 					</a>
 				</div>
 			</div>
 		</fieldset>
 	</div>
-<?php else :
+<?php } else {
 	$account_required            = job_manager_user_requires_account();
 	$registration_enabled        = job_manager_enable_registration();
 	$registration_fields         = wpjm_get_registration_fields();
@@ -49,26 +47,26 @@ if ( is_user_logged_in() ) : ?>
 	?>
 	<div class="pyj_login_form">
 		<fieldset class="fieldset-login_required">
-			<label><?php esc_html_e( 'Have an account?', 'wp-job-manager' ); ?></label>
+			<label><?php esc_html_e( 'Have an account?', 'marketingops' ); ?></label>
 			<div class="field account-sign-in">
 				<label>
 					<?php if ( $registration_enabled ) : ?>
 
-						<?php printf( esc_html__( 'If you don\'t have an account you can create one below by entering your email address/username.', 'wp-job-manager' ), $account_required ? '' : esc_html__( 'optionally', 'wp-job-manager' ) . ' ' ); ?>
+						<?php printf( esc_html__( 'If you don\'t have an account you can create one below by entering your email address/username.', 'marketingops' ), $account_required ? '' : esc_html__( 'optionally', 'marketingops' ) . ' ' ); ?>
 						<?php if ( $use_standard_password_email ) : ?>
-							<?php printf( esc_html__( 'Your account details will be confirmed via email.', 'wp-job-manager' ) ); ?>
+							<?php printf( esc_html__( 'Your account details will be confirmed via email.', 'marketingops' ) ); ?>
 						<?php endif; ?>
 
 					<?php elseif ( $account_required ) : ?>
 
-						<?php echo wp_kses_post( apply_filters( 'submit_job_form_login_required_message',  __( 'You must sign in to create a new listing.', 'wp-job-manager' ) ) ); ?>
+						<?php echo wp_kses_post( apply_filters( 'submit_job_form_login_required_message',  __( 'You must sign in to create a new listing.', 'marketingops' ) ) ); ?>
 
 					<?php endif; ?>
 				</label>
 				<div class="pyj_login_form_btn">
-					<h4>Already a member?</h4>
-					<a class="button" href="<?php echo esc_url( apply_filters( 'submit_job_form_login_url', home_url( '/log-in?redirect_to=/post-your-marketing-operations-jobs' ) ) ); ?>">
-						<span class="form_btn_text"><?php esc_html_e( 'Sign in', 'wp-job-manager' ); ?></span>
+					<h4><?php esc_html_e( 'Already a member?', 'marketingops' ); ?></h4>
+					<a class="button" href="<?php echo esc_url( apply_filters( 'submit_job_form_login_url', home_url( "/log-in?redirect_to=/{$current_page_slug}/" ) ) ); ?>">
+						<span class="form_btn_text"><?php esc_html_e( 'Sign in', 'marketingops' ); ?></span>
 						<span class="form_btn_svg"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="11" viewBox="0 0 20 11" fill="none"><path d="M14.7859 0.74192C14.4643 0.729551 14.1659 0.913544 14.0345 1.20731C13.9015 1.50109 13.9587 1.84433 14.1814 2.07935L16.6135 4.70782H1.30494C1.0189 4.70318 0.754506 4.85316 0.610713 5.10055C0.465374 5.34639 0.465374 5.65253 0.610713 5.89837C0.754506 6.14575 1.0189 6.29573 1.30494 6.29109H16.6135L14.1814 8.91957C13.9835 9.12675 13.9139 9.42361 13.9974 9.69728C14.0809 9.97096 14.3051 10.1781 14.5834 10.24C14.8632 10.3018 15.1539 10.2075 15.3441 9.99569L19.5017 5.49946L15.3441 1.00322C15.2018 0.845513 14.9993 0.749651 14.7859 0.74192Z" fill="white"></path></svg></span>
 					</a>
 				</div>
@@ -80,7 +78,7 @@ if ( is_user_logged_in() ) : ?>
 				?>
 				<fieldset class="fieldset-<?php echo esc_attr( $key ); ?>">
 					<label
-						for="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $field[ 'label' ] ) . wp_kses_post( apply_filters( 'submit_job_form_required_label', $field[ 'required' ] ? '' : ' <small>' . __( '(optional)', 'wp-job-manager' ) . '</small>', $field ) ); ?></label>
+						for="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $field[ 'label' ] ) . wp_kses_post( apply_filters( 'submit_job_form_required_label', $field[ 'required' ] ? '' : ' <small>' . __( '(optional)', 'marketingops' ) . '</small>', $field ) ); ?></label>
 					<div class="field <?php echo $field[ 'required' ] ? 'required-field draft-required' : ''; ?>">
 						<?php get_job_manager_template( 'form-fields/' . $field[ 'type' ] . '-field.php', [ 'key'   => $key, 'field' => $field ] ); ?>
 					</div>
@@ -91,7 +89,7 @@ if ( is_user_logged_in() ) : ?>
 		}
 		?>
 	</div>
-<?php endif; ?>
+<?php } ?>
 
 <div class="pyj_desc">
 	<div class="pyj_desc_box">
