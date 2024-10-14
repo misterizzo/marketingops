bd695df1722b90ba66265ab20eb2fdedb29c7c8f
Adarsh Verma <adarsh@concatstring.com>
2024-10-14 17:21:17 +0530

    troubleshooting job posting issue

--- wp-content/themes/marketingops/job_manager/account-signin.php
+++ wp-content/themes/marketingops/job_manager/account-signin.php
@@ -53,7 +53,6 @@ if ( is_user_logged_in() ) { ?>
 			<div class="field account-sign-in">
 				<label><?php echo wp_kses_post( apply_filters( 'submit_job_form_login_required_message',  __( 'You must sign in to create a new listing.', 'marketingops' ) ) ); ?></label>
 				<div class="pyj_login_form_btn">
-					<h4><?php esc_html_e( 'Already a member?', 'marketingops' ); ?></h4>
 					<a class="button" href="<?php echo esc_url( apply_filters( 'submit_job_form_login_url', home_url( "/log-in?redirect_to=/{$current_page_slug}/" ) ) ); ?>">
 						<span class="form_btn_text"><?php esc_html_e( 'Sign in', 'marketingops' ); ?></span>
 						<span class="form_btn_svg"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="11" viewBox="0 0 20 11" fill="none"><path d="M14.7859 0.74192C14.4643 0.729551 14.1659 0.913544 14.0345 1.20731C13.9015 1.50109 13.9587 1.84433 14.1814 2.07935L16.6135 4.70782H1.30494C1.0189 4.70318 0.754506 4.85316 0.610713 5.10055C0.465374 5.34639 0.465374 5.65253 0.610713 5.89837C0.754506 6.14575 1.0189 6.29573 1.30494 6.29109H16.6135L14.1814 8.91957C13.9835 9.12675 13.9139 9.42361 13.9974 9.69728C14.0809 9.97096 14.3051 10.1781 14.5834 10.24C14.8632 10.3018 15.1539 10.2075 15.3441 9.99569L19.5017 5.49946L15.3441 1.00322C15.2018 0.845513 14.9993 0.749651 14.7859 0.74192Z" fill="white"></path></svg></span>
@@ -61,22 +60,6 @@ if ( is_user_logged_in() ) { ?>
 				</div>
 			</div>
 		</fieldset>
-		<?php
-		if ( ! empty( $registration_fields ) ) {
-			foreach ( $registration_fields as $key => $field ) {
-				?>
-				<fieldset class="fieldset-<?php echo esc_attr( $key ); ?>">
-					<label
-						for="<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $field[ 'label' ] ) . wp_kses_post( apply_filters( 'submit_job_form_required_label', $field[ 'required' ] ? '' : ' <small>' . __( '(optional)', 'marketingops' ) . '</small>', $field ) ); ?></label>
-					<div class="field <?php echo $field[ 'required' ] ? 'required-field draft-required' : ''; ?>">
-						<?php get_job_manager_template( 'form-fields/' . $field[ 'type' ] . '-field.php', [ 'key'   => $key, 'field' => $field ] ); ?>
-					</div>
-				</fieldset>
-				<?php
-			}
-			do_action( 'job_manager_register_form' );
-		}
-		?>
 	</div>
 <?php } ?>
 
