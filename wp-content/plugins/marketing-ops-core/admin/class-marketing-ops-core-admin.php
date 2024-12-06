387537b561098f60e3abb7bcf5d0c3d745819062
Adarsh Verma <adarsh.srmcem@gmail.com>
2024-12-06 11:34:22 +0530

    working on https://app.clickup.com/t/868ar2cyp

--- wp-content/plugins/marketing-ops-core/admin/class-marketing-ops-core-admin.php
+++ wp-content/plugins/marketing-ops-core/admin/class-marketing-ops-core-admin.php
@@ -1494,12 +1494,9 @@ class Marketing_Ops_Core_Admin {
 		$post_type                 = filter_input( INPUT_GET, 'post_type', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
 		$selected_additional_label = filter_input( INPUT_GET, 'additional_label', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
 
-		var_dump( $selected_additional_label );
-		die("pool");
-
 		// Only for the pages.
 		if ( 'page' === $post_type && is_admin() && 'edit.php' === $pagenow && ! is_null( $selected_additional_label ) ) {
-			if ( -1 !== $selected_additional_label ) {
+			if ( '-1' !== $selected_additional_label ) {
 				$query->query_vars['meta_query'][] = array(
 					'key'     => 'additional_label',
 					'value'   => sprintf( ':"%s";', $selected_additional_label ),
