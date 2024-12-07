ff819485e354a3baa87a257c7d665b78cbfa8c08
meggira <kumbharanameghna@gmail.com>
2024-12-07 14:55:43 +0530

    image height width fixed

--- wp-content/plugins/marketing-ops-core/public/partials/templates/woocommerce/myaccount/agency-profile.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/woocommerce/myaccount/agency-profile.php
@@ -331,7 +331,7 @@ if ( false ) {
 				<div class="upload-btn-wrapper">
 					<button class="btn">Select an image</button>
 					<p>For the best results, crop your photo to 640 x 380px before uploading.</p>
-					<input class="imageInput" onchange="readURL(this)" accept="image/*" />
+					<input type="file" class="imageInput" onchange="readURL(this)" accept="image/*" />
 					<div id="previewContainer" class="preview-container" style="display: none;">
     						<img class="preview-image" src="#" alt="Image Preview" />
     						<button class="removePreview" id="removePreview" onclick="removePreview()"><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16" cy="16" r="15.35" stroke="white" stroke-width="1.3"></circle><path d="M11 11L16 16L11 21" stroke="white" stroke-width="1.3"></path><path d="M21 11L16 16L21 21" stroke="white" stroke-width="1.3"></path></svg></button>
