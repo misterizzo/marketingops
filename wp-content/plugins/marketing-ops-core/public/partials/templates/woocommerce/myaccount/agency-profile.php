2411015852516980a1ff0f32ac06c4d06e036157
meggira <kumbharanameghna@gmail.com>
2024-12-17 12:16:00 +0530

    blur imga

--- wp-content/plugins/marketing-ops-core/public/partials/templates/woocommerce/myaccount/agency-profile.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/woocommerce/myaccount/agency-profile.php
@@ -33,7 +33,9 @@ $agency_id        = ( ! empty( $agency_id->posts[0] ) ) ? $agency_id->posts[0] :
 if ( false === $agency_id || false === $is_agency_member ) {
 	?><div class="bluredpage">
 		<h5>Coming soon</h5>
+		<div class="bluredpageinner">
 		<?php echo do_shortcode( '[elementor-template id="231177"]' ); ?>
+</div>
 	</div><?	
 } else {
 	$agency_title       = get_the_title( $agency_id );
