7a9bcdcccf44ac06e2a50c4a65269d5c39055294
Adarsh Verma <adarsh.srmcem@gmail.com>
2025-01-16 01:20:18 +0530

    testing agency profile on customer dashboard

--- wp-content/plugins/marketing-ops-core/public/partials/templates/woocommerce/myaccount/agency-profile.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/woocommerce/myaccount/agency-profile.php
@@ -31,14 +31,12 @@ $agency_id        = ( ! empty( $agency_id->posts[0] ) ) ? $agency_id->posts[0] :
 
 // If the linked agency is not available, show the registration page.
 if ( false === $agency_id || false === $is_agency_member ) {
-	echo 'here if';
 	?>
 	<div class="bluredpage">
 		<h5><?php echo wp_kses_post( 'This page will be available soon. In the meantime if you would like to create an agency listing please use our <a href="/contact/">contact us form</a> and our team can add you to the directory. Thank you!', 'marketingops' ); ?></h5>
 		<div class="bluredpageinner"><?php echo do_shortcode( '[elementor-template id="231177"]' ); ?></div>
 	</div><?php
 } else {
-	echo 'here else';
 	$agency_post_status = get_post_status( $agency_id );
 	$agency_title       = get_the_title( $agency_id );
 	$agency_image_id    = get_post_thumbnail_id( $agency_id );
