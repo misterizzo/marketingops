cc0c356f6dfa3110028149c082456ab4941d7e18
Adarsh Verma <adarsh.srmcem@gmail.com>
2025-02-11 20:27:37 +0530

    implement google recaptcha in login page

--- wp-content/plugins/marketing-ops-core/public/class-marketing-ops-core-public.php
+++ wp-content/plugins/marketing-ops-core/public/class-marketing-ops-core-public.php
@@ -3588,8 +3588,6 @@ class Marketing_Ops_Core_Public {
 		);
 		$user_signon_response = $user_signon->errors;
 
-		debug( $user_signon_response ); die;
-
 		if ( empty( $user_signon_response )  ) {
 			$user_response_msg = __( 'You are successfully logged in.', 'marketingops' );
 			$message           = 'moc-successfully-login';
