3f9e4186c521fdcd0af80cc34b16eaf988fd9020
Adarsh Verma <adarsh.srmcem@gmail.com>
2025-02-11 15:48:06 +0530

    implement google recaptcha in login page

--- wp-content/plugins/marketing-ops-core/public/js/marketing-ops-core-public.js
+++ wp-content/plugins/marketing-ops-core/public/js/marketing-ops-core-public.js
@@ -2668,12 +2668,10 @@ jQuery( document ).ready( function( $ ) {
 	/**
 	 * Render Google Recaptcha on the login form.
 	 */
-	if ( $( '#google-recaptcha' ).length ) {
-		grecaptcha.render( 'google-recaptcha', {
-			'sitekey' : '6LeKbKcqAAAAAKObwIyMaqz3UTNVVf8j_Ryt15De',
-			'theme': 'light',
-		} );
-	}
+	grecaptcha.render( 'google-recaptcha', {
+		'sitekey' : '6LeKbKcqAAAAAKObwIyMaqz3UTNVVf8j_Ryt15De',
+		'theme': 'light',
+	} );
 
 	/**
 	 * jQuery to run ajax for user login process.
