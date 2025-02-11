2f182d02baa61ee943f7f44694c7b8869881db96
Adarsh Verma <adarsh.srmcem@gmail.com>
2025-02-11 20:27:10 +0530

    implement google recaptcha in login page

--- wp-content/plugins/marketingops-google-recaptcha/includes/class-cf-core-functions-public.php
+++ wp-content/plugins/marketingops-google-recaptcha/includes/class-cf-core-functions-public.php
@@ -35,7 +35,7 @@ class Cf_Core_Functions_Public {
 		add_action( 'woocommerce_lostpassword_form', array( $this, 'cf_woocommerce_lostpassword_form_callback' ) );
 		// add_action( 'woocommerce_review_order_before_payment', array( $this, 'cf_woocommerce_review_order_before_payment_callback' ) );
 		add_action( 'woocommerce_register_post', array( $this, 'cf_woocommerce_register_post_callback' ), 99, 3 );
-		add_action( 'authenticate', array( $this, 'cf_authenticate_callback' ), 99, 3 );
+		// add_action( 'authenticate', array( $this, 'cf_authenticate_callback' ), 99, 3 );
 		add_action( 'lostpassword_post', array( $this, 'cf_lostpassword_post_callback' ), 99 );
 		add_action( 'woocommerce_checkout_process', array( $this, 'cf_woocommerce_checkout_process_callback' ), 99 );
 		add_action( 'wp_ajax_save_payment_data', array( $this, 'cf_save_payment_data_callback' ) );
