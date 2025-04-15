aade26c5126e8843e05595439623721b3f0a9aa1
meggira <kumbharanameghna@gmail.com>
2025-04-16 00:45:31 +0530

    gradient on H1

--- wp-content/plugins/marketing-ops-core/public/partials/templates/users/login.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/users/login.php
@@ -18,18 +18,34 @@ get_header();
 
 if ( is_user_logged_in() ) {
 	?>
-	<div class="elementor-column elementor-col-100 elementor-top-column elementor-element register_content">
-		<div class="elementor-widget-wrap elementor-element-populated">
-			<div class="elementor-element profileheading gradient-title elementor-widget elementor-widget-heading">
-				<div class="elementor-widget-container">
-					<h2 class="elementor-heading-title elementor-size-default"><?php esc_html_e( 'You are already loggedin!', 'marketingops' ); ?></h2>
+	<div class="elementor-column elementor-col-100 elementor-top-column elementor-element register_content login_content moc_login_form_section register_page login_page elementor-section elementor-section-boxed">
+		<div class="elementor-widget-wrap elementor-element-populated moc-register-container elementor-container elementor-column-gap-default">
+			<div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-715834fb register_content">
+				<div class="register_content login_content moc_login_form_section">
+					<div class="elementor-column elementor-col-100 elementor-top-column elementor-element register_content">
+						<div class="elementor-widget-wrap elementor-element-populated">
+							<div class="elementor-element profileheading gradient-title elementor-widget elementor-widget-heading">
+								<div class="elementor-widget-container">
+									<h2 class="elementor-heading-title elementor-size-default"><?php esc_html_e( 'You are already loggedin!', 'marketingops' ); ?></h2>
+								</div>
+							</div>
+							<div class="elementor-element profileheading elementor-widget elementor-widget-button">
+								<div class="elementor-widget-container">
+									<a href="<?php echo esc_url( home_url( 'profile' ) ); ?>" class="elementor-button elementor-size-default"><?php esc_html_e( 'Go to my profile', 'marketingops' ); ?></a>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div class="elementor-element profileheading elementor-widget elementor-widget-button">
+			<div class="elementor-column elementor-col-50 elementor-top-column elementor-element elementor-element-ea2a0bd register_img">
+				<div class="elementor-widget-wrap elementor-element-populated">
+					<div class="elementor-element elementor-element-5dd7f05 elementor-widget elementor-widget-image" data-id="5dd7f05" data-element_type="widget" data-widget_type="image.default">
 				<div class="elementor-widget-container">
-					<a href="<?php echo esc_url( home_url( 'profile' ) ); ?>" class="elementor-button elementor-size-default"><?php esc_html_e( 'Go to my profile', 'marketingops' ); ?></a>
+													<img fetchpriority="high" decoding="async" width="455" height="455" src="https://marketingops.com/wp-content/uploads/2022/04/Asset-1-3.png" class="attachment-large size-large wp-image-165577" alt="Asset 1 3" srcset="https://marketingops.com/wp-content/uploads/2022/04/Asset-1-3.png 455w, https://marketingops.com/wp-content/uploads/2022/04/Asset-1-3-300x300.png 300w, https://marketingops.com/wp-content/uploads/2022/04/Asset-1-3-150x150.png 150w, https://marketingops.com/wp-content/uploads/2022/04/Asset-1-3-100x100.png 100w" sizes="(max-width: 455px) 100vw, 455px" title="Log In">													</div>
 				</div>
-			</div>
+				</div>	
+			</div>	
 		</div>
 	</div>
 	<?php
